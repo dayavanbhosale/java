bec220c99808b3eb86a9eee284d88d7abbdc2d55
dayavanbhosale <105915770+dayavanbhosale@users.noreply.github.com>
2022-05-30 11:09:25 +0530

    Add files via upload

---
+++ lab Activity/EvenNumbers.java
@@ -0,0 +1,19 @@
+package Dayavan;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//number
+		int n = 20;
+		
+		//print all even numbers <=n 
+		for (int i=2; i<=n; i+=2) {
+			System.out.print(i+"  ");
+		}
+	}
+	
+
+}
+
